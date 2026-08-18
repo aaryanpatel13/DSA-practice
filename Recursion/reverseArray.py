cd672c6fda6463697daeca0ab29fdfdcf13833cb
aryanpatel10 <aaryanpatel013@gmail.com>
2026-08-18 14:26:32 +0530

    reverse the givwn array

--- Recursion/reverseArray.py
+++ Recursion/reverseArray.py
@@ -1,14 +1,14 @@
 # Recursive function using two pointers
-# def reverseArray(nums,l,r):
-#     if l > r :
-#         return
-#     nums[l], nums[r] = nums[r], nums[l]
-#     reverseArray(nums,l+1,r-1)
+def reverseArray(nums,l,r):
+    if l > r :
+        return
+    nums[l], nums[r] = nums[r], nums[l]
+    reverseArray(nums,l+1,r-1)
 
-# #TEST CASE
-# nums = [1,2,3,4]
-# reverseArray(nums,0,len(nums)-1)
-# print(nums)
+#TEST CASE
+nums = [1,2,3,4]
+reverseArray(nums,0,len(nums)-1)
+print(nums)
 
 
 # Recursive function using one pointer
