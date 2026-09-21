30a5d86c0654f0ab27da9faefe5029ee8c5aed04
aryanpatel10 <aaryanpatel013@gmail.com>
2026-09-21 14:33:59 +0530

    Search insert position

---
+++ 05_Binary_Search/searchInsert.py
@@ -0,0 +1,32 @@
+#                           35. Search Insert Position
+"""
+Given a sorted array of distinct integers and a target value, 
+return the index if the target is found. If not, return the index where it would be if 
+it were inserted in order.
+You must write an algorithm with O(log n) runtime complexity.
+
+Input: nums = [1,3,5,6], target = 5
+Output: 2
+
+"""
+
+def searchInsert(nums,target):
+    n = len(nums)
+    low = 0
+    high = n-1
+    ans = n    
+    while low <= high:
+        mid = (low + high)//2
+        
+        if nums[mid] == target:
+            return mid
+        elif nums[mid] > target:
+            ans = mid
+            high = mid -1
+        else:
+            low = mid + 1
+    return ans
+
+nums = [1,3,5,6]
+target = 4
+print(searchInsert(nums,target))
